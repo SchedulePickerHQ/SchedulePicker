51d7c29b9eb1f56c34f6a60e665374d89876476f
Atsusuke <atsusuke-murata@cybozu.co.jp>
2021-09-11 17:46:31 +0900

    Implement function to post schedule as markdown.

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,5 +1,6 @@
 import browser from 'webextension-polyfill';
 import { ActionMessage, ACTION_MESSAGE_ID } from './actions/abstract-action';
+import { assertExists } from './utils/asserts';
 import { LOADING_STATUS, showLoading } from './utils/loading';
 
 browser.runtime.onMessage.addListener((actionMessage: ActionMessage) => {
@@ -8,10 +9,47 @@ browser.runtime.onMessage.addListener((actionMessage: ActionMessage) => {
             showLoading(actionMessage.message === LOADING_STATUS.SHOW);
             break;
         case ACTION_MESSAGE_ID.SCHEDULE_EVENTS:
-            // TODO: execCommand は廃止されるので別の方法を考える
-            document.execCommand('insertText', false, actionMessage.message);
+            assertExists(document.activeElement);
+            insertTextAtCaret(document.activeElement as HTMLElement, actionMessage.message, 'markdown');
             break;
         default:
             throw new Error('Not found action message id');
     }
 });
+
+const isTextAreaOrInputEl = (target: HTMLElement): target is HTMLInputElement | HTMLTextAreaElement =>
+    target.tagName === 'INPUT' || target.tagName === 'TEXTAREA';
+
+const insertTextAtCaret = (target: HTMLElement, text: string, syntax: 'html' | 'markdown') => {
+    if (isTextAreaOrInputEl(target)) {
+        const selectionStart = target.selectionStart;
+        const selectionEnd = target.selectionEnd;
+        assertExists(selectionStart);
+        assertExists(selectionEnd);
+        const startText = target.value.slice(0, selectionStart);
+        const endText = target.value.slice(selectionEnd);
+
+        if (syntax === 'html') {
+            // TODO: text の div をパースする
+        } else {
+            target.value = startText + text + endText;
+        }
+
+        // textarea に入力される文字列をリアルタイムで状態管理しているようなページだと、
+        // 拡張機能側で textarea の value を変更しても change イベントが発火せず、再レンダリングしたときに
+        // ページ側で管理している状態で TextArea が上書きされてしまうので、能動的に change イベントを発火させる。
+        target.dispatchEvent(new window.Event('change', { bubbles: true }));
+    } else if (target.isContentEditable) {
+        const selection = document.getSelection();
+        assertExists(selection);
+        const range = selection.getRangeAt(0);
+        range.deleteContents();
+
+        const node = document.createElement('div');
+        node.style.whiteSpace = 'pre';
+        node.innerHTML = text;
+        range.insertNode(node);
+
+        // TODO: 貼り付けたものが選択状態になったままになっている
+    }
+};
