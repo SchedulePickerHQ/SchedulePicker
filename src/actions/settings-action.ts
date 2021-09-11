b63fe08ddeec1a17fca31f44cb98eea2cdb1161c
Atsusuke <atsusuke-murata@cybozu.co.jp>
2021-09-12 00:05:58 +0900

    Open options page.

--- src/actions/settings-action.ts
+++ src/actions/settings-action.ts
@@ -1,8 +1,8 @@
-import { Menus, Tabs } from 'webextension-polyfill';
+import browser, { Menus, Tabs } from 'webextension-polyfill';
 import { AbstractAction } from './base/abstract-action';
 
 export class SettingsAction extends AbstractAction {
-    execute(_info: Menus.OnClickData, _tab: Tabs.Tab) {
-        // TODO: オプション画面を別タブで開く
+    async execute(_info: Menus.OnClickData, _tab: Tabs.Tab) {
+        await browser.runtime.openOptionsPage();
     }
 }
