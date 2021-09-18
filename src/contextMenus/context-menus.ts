5731f9feb6bd56e1afc95dd93a6875d993b24254
Atsusuke <atsusuke-murata@cybozu.co.jp>
2021-09-19 00:58:02 +0900

    Synchronize context menu and option settings with context menu displayed that saved in storage.

--- src/contextMenus/context-menus.ts
+++ src/contextMenus/context-menus.ts
@@ -1,5 +1,5 @@
 import browser from 'webextension-polyfill';
-import { getSyntax } from '../storage/storage';
+import { getSyntax, getContextMenuDisplayed } from '../storage/storage';
 import { ContextMenuBuilder, ContextMenuItem } from './context-menu-builder';
 
 const createContextMenu = (items: ContextMenuItem[]) => {
@@ -12,15 +12,43 @@ const removeAllContextMenu = async () => browser.contextMenus.removeAll();
 
 export const buildContextMenu = async () => {
     await removeAllContextMenu();
-    const syntax = await getSyntax();
-    const htmlSelected = syntax === 'html';
-    const markdownSelected = syntax === 'markdown';
-    const items = new ContextMenuBuilder()
-        .addToday()
-        .addNextBusinessDay()
-        .addHtml(htmlSelected)
-        .addMarkdown(markdownSelected)
-        .addSettings()
-        .build();
-    createContextMenu(items);
+    const builder = new ContextMenuBuilder();
+    const display = await getContextMenuDisplayed();
+
+    if (display.today) {
+        builder.addToday();
+    }
+
+    if (display.tomorrow) {
+        builder.addTomorrow();
+    }
+
+    if (display.yesterday) {
+        builder.addYesterDay();
+    }
+
+    if (display.nextBusinessDay) {
+        builder.addNextBusinessDay();
+    }
+
+    if (display.previousBusinessDay) {
+        builder.addPreviousBusinessDay();
+    }
+
+    if (display.specifiedDay) {
+        builder.addSpecifiedDay();
+    }
+
+    if (display.template) {
+        builder.addTemplate();
+    }
+
+    if (display.syntax) {
+        const syntax = await getSyntax();
+        builder.addHtml(syntax === 'html');
+        builder.addMarkdown(syntax === 'markdown');
+    }
+
+    builder.addSettings();
+    createContextMenu(builder.build());
 };
