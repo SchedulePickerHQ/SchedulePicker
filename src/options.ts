bd738bfc276b9c7b9cd09dfe9cb043ce0d21d3d8
Atsusuke <atsusuke-murata@cybozu.co.jp>
2021-09-18 23:31:54 +0900

    Save context menu display settings.

--- src/options.ts
+++ src/options.ts
@@ -1,15 +1,15 @@
-import { isButtonElement } from './utils/element-type-check';
+import { setContextMenuDisplayed } from './storage/storage';
+import { assert } from './utils/asserts';
+import { isButtonElement, isInputElement } from './utils/element-type-check';
 
 window.addEventListener('DOMContentLoaded', () => {
     const saveButton = document.querySelector('.save-button');
+    assert(isButtonElement(saveButton));
 
-    if (!isButtonElement(saveButton)) {
-        throw new Error('Not button element');
-    }
-
-    saveButton.addEventListener('click', () => {
+    saveButton.addEventListener('click', async () => {
         saveButton.disabled = true;
         saveButton.classList.add('saving');
+        await saveContextMenuDisplayed();
 
         setTimeout(() => {
             saveButton.classList.remove('saving');
@@ -17,3 +17,34 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 3000);
     });
 });
+
+const saveContextMenuDisplayed = async () => {
+    const todayInput = document.querySelector('#today');
+    const tomorrowInput = document.querySelector('#tomorrow');
+    const yesterdayInput = document.querySelector('#yesterday');
+    const nextBusinessDayInput = document.querySelector('#next-business-day');
+    const previousBusinessDayInput = document.querySelector('#previous-business-day');
+    const specifiedDayInput = document.querySelector('#specified-day');
+    const templateInput = document.querySelector('#template');
+    const syntaxInput = document.querySelector('#syntax');
+
+    assert(isInputElement(todayInput));
+    assert(isInputElement(tomorrowInput));
+    assert(isInputElement(yesterdayInput));
+    assert(isInputElement(nextBusinessDayInput));
+    assert(isInputElement(previousBusinessDayInput));
+    assert(isInputElement(specifiedDayInput));
+    assert(isInputElement(templateInput));
+    assert(isInputElement(syntaxInput));
+
+    await setContextMenuDisplayed({
+        today: todayInput.checked,
+        tomorrow: tomorrowInput.checked,
+        yesterday: yesterdayInput.checked,
+        nextBusinessDay: nextBusinessDayInput.checked,
+        previousBusinessDay: previousBusinessDayInput.checked,
+        specifiedDay: specifiedDayInput.checked,
+        template: templateInput.checked,
+        syntax: syntaxInput.checked,
+    });
+};
--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -2,18 +2,41 @@ import browser from 'webextension-polyfill';
 
 type Syntax = 'html' | 'markdown';
 
+type ContextMenuDisplayed = {
+    today: boolean;
+    tomorrow: boolean;
+    yesterday: boolean;
+    nextBusinessDay: boolean;
+    previousBusinessDay: boolean;
+    specifiedDay: boolean;
+    template: boolean;
+    syntax: boolean;
+};
+
 type StorageInitValue = {
     SYNTAX: Syntax;
+    CONTEXT_MENU_DISPLAYED: ContextMenuDisplayed;
     TEMPLATE_TEXT: string;
 };
 
 const STORAGE_KEY = {
     SYNTAX: 'syntax',
+    CONTEXT_MENU_DISPLAYED: 'contextMenuDisplayed',
     TEMPLATE_TEXT: 'templateText',
 } as const;
 
 const STORAGE_INIT_VALUE: StorageInitValue = {
     SYNTAX: 'html',
+    CONTEXT_MENU_DISPLAYED: {
+        today: true,
+        tomorrow: false,
+        yesterday: false,
+        nextBusinessDay: false,
+        previousBusinessDay: false,
+        specifiedDay: false,
+        template: false,
+        syntax: true,
+    },
     TEMPLATE_TEXT: '', // TODO: 利用者が理解しやすい初期値を考える
 };
 
@@ -29,4 +52,18 @@ export const getSyntax = async (): Promise<Syntax> => {
     }
 
     return item[STORAGE_KEY.SYNTAX] as Syntax;
+};
+
+export const setContextMenuDisplayed = async (displayed: ContextMenuDisplayed) => {
+    await browser.storage.sync.set({ [STORAGE_KEY.CONTEXT_MENU_DISPLAYED]: displayed });
+};
+
+export const getContextMenuDisplayed = async (): Promise<ContextMenuDisplayed> => {
+    const item = await browser.storage.sync.get(STORAGE_KEY.CONTEXT_MENU_DISPLAYED);
+
+    if (Object.keys(item).length === 0) {
+        return STORAGE_INIT_VALUE.CONTEXT_MENU_DISPLAYED;
+    }
+
+    return item[STORAGE_KEY.CONTEXT_MENU_DISPLAYED] as ContextMenuDisplayed;
 };
