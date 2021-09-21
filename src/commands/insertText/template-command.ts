4400e4ce6802dd49fcb701adca29e516443ac6f7
Atsusuke <atsusuke-murata@cybozu.co.jp>
2021-09-22 00:02:56 +0900

    Fix special template character.

--- src/commands/insertText/template-command.ts
+++ src/commands/insertText/template-command.ts
@@ -2,21 +2,23 @@ import { searchNextBusinessDateTime, searchPreviousBusinessDateTime } from '../.
 import { getScheduleEvents } from '../../garoon/schedule';
 import { getSyntax, getTemplateText } from '../../storage/storage';
 import { SyntaxFactory } from '../../syntax/syntax-factory';
-import { createEndOfTime, createStartOfTime, getNowDateTime } from '../../utils/date-time';
+import { createEndOfTime, createStartOfTime, getNowDateTime, formatDateTime } from '../../utils/date-time';
 import { InsertTextCommand } from './insert-text-command';
 
-const SPECIAL_TEMPLATE_CHARACTOR = {
-    TODAY_TITLE: '{%TODAY_TITLE%}',
-    TOMORROW_TITLE: '{%TOMORROW_TITLE%}',
-    YESTERDAY_TITLE: '{%YESTERDAY_TITLE%}',
-    NEXT_BUSINESS_DAY_TITLE: '{%NEXT_BUSINESS_DAY_TITLE%}',
-    PREVIOUS_BUSINESS_DAY_TITLE: '{%PREVIOUS_BUSINESS_DAY_TITLE%}',
+const SPECIAL_TEMPLATE_CHARACTER = {
     TODAY: '{%TODAY%}',
     TOMORROW: '{%TOMORROW%}',
     YESTERDAY: '{%YESTERDAY%}',
     NEXT_BUSINESS_DAY: '{%NEXT_BUSINESS_DAY%}',
     PREVIOUS_BUSINESS_DAY: '{%PREVIOUS_BUSINESS_DAY%}',
+    TODAY_EVENTS: '{%TODAY_EVENTS%}',
+    TOMORROW_EVENTS: '{%TOMORROW_EVENTS%}',
+    YESTERDAY_EVENTS: '{%YESTERDAY_EVENTS%}',
+    NEXT_BUSINESS_DAY_EVENTS: '{%NEXT_BUSINESS_DAY_EVENTS%}',
+    PREVIOUS_BUSINESS_DAY_EVENTS: '{%PREVIOUS_BUSINESS_DAY_EVENTS%}',
 } as const;
+
+const DATE_FORMAT = 'YYYY-MM-DD';
 
 export class TemplateCommand extends InsertTextCommand {
     protected async createInsertText(domain: string): Promise<string | null> {
@@ -24,60 +26,60 @@ export class TemplateCommand extends InsertTextCommand {
         const factory = new SyntaxFactory().create(syntax);
         let templateText = await getTemplateText();
 
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.TODAY_TITLE)) {
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.TODAY)) {
             const dateTime = getNowDateTime();
-            const title = factory.createTitle(dateTime);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.TODAY_TITLE, title);
+            const title = formatDateTime(dateTime, DATE_FORMAT);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.TODAY, title);
         }
 
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.TOMORROW_TITLE)) {
-            const dateTime = getNowDateTime();
-            dateTime.date += 1;
-            const title = factory.createTitle(dateTime);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.TOMORROW_TITLE, title);
-        }
-
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.YESTERDAY_TITLE)) {
-            const dateTime = getNowDateTime();
-            dateTime.date -= 1;
-            const title = factory.createTitle(dateTime);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.YESTERDAY_TITLE, title);
-        }
-
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.NEXT_BUSINESS_DAY_TITLE)) {
-            const dateTime = await searchNextBusinessDateTime(domain);
-            const title = factory.createTitle(dateTime);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.NEXT_BUSINESS_DAY_TITLE, title);
-        }
-
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.PREVIOUS_BUSINESS_DAY_TITLE)) {
-            const dateTime = await searchPreviousBusinessDateTime(domain);
-            const title = factory.createTitle(dateTime);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.PREVIOUS_BUSINESS_DAY_TITLE, title);
-        }
-
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.TODAY)) {
-            const dateTime = getNowDateTime();
-            const events = await getScheduleEvents(domain, {
-                startTime: createStartOfTime(dateTime),
-                endTime: createEndOfTime(dateTime),
-            });
-            const schedule = factory.createEvents(events);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.TODAY, schedule);
-        }
-
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.TOMORROW)) {
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.TOMORROW)) {
             const dateTime = getNowDateTime();
             dateTime.date += 1;
+            const title = formatDateTime(dateTime, DATE_FORMAT);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.TOMORROW, title);
+        }
+
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.YESTERDAY)) {
+            const dateTime = getNowDateTime();
+            dateTime.date -= 1;
+            const title = formatDateTime(dateTime, DATE_FORMAT);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.YESTERDAY, title);
+        }
+
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.NEXT_BUSINESS_DAY)) {
+            const dateTime = await searchNextBusinessDateTime(domain);
+            const title = formatDateTime(dateTime, DATE_FORMAT);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.NEXT_BUSINESS_DAY, title);
+        }
+
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.PREVIOUS_BUSINESS_DAY)) {
+            const dateTime = await searchPreviousBusinessDateTime(domain);
+            const title = formatDateTime(dateTime, DATE_FORMAT);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.PREVIOUS_BUSINESS_DAY, title);
+        }
+
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.TODAY_EVENTS)) {
+            const dateTime = getNowDateTime();
             const events = await getScheduleEvents(domain, {
                 startTime: createStartOfTime(dateTime),
                 endTime: createEndOfTime(dateTime),
             });
             const schedule = factory.createEvents(events);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.TOMORROW, schedule);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.TODAY_EVENTS, schedule);
         }
 
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.YESTERDAY)) {
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.TOMORROW_EVENTS)) {
+            const dateTime = getNowDateTime();
+            dateTime.date += 1;
+            const events = await getScheduleEvents(domain, {
+                startTime: createStartOfTime(dateTime),
+                endTime: createEndOfTime(dateTime),
+            });
+            const schedule = factory.createEvents(events);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.TOMORROW_EVENTS, schedule);
+        }
+
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.YESTERDAY_EVENTS)) {
             const dateTime = getNowDateTime();
             dateTime.date -= 1;
             const events = await getScheduleEvents(domain, {
@@ -85,27 +87,27 @@ export class TemplateCommand extends InsertTextCommand {
                 endTime: createEndOfTime(dateTime),
             });
             const schedule = factory.createEvents(events);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.YESTERDAY, schedule);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.YESTERDAY_EVENTS, schedule);
         }
 
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.NEXT_BUSINESS_DAY)) {
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.NEXT_BUSINESS_DAY_EVENTS)) {
             const dateTime = await searchNextBusinessDateTime(domain);
             const events = await getScheduleEvents(domain, {
                 startTime: createStartOfTime(dateTime),
                 endTime: createEndOfTime(dateTime),
             });
             const schedule = factory.createEvents(events);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.NEXT_BUSINESS_DAY, schedule);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.NEXT_BUSINESS_DAY_EVENTS, schedule);
         }
 
-        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTOR.PREVIOUS_BUSINESS_DAY)) {
+        if (templateText.includes(SPECIAL_TEMPLATE_CHARACTER.PREVIOUS_BUSINESS_DAY_EVENTS)) {
             const dateTime = await searchPreviousBusinessDateTime(domain);
             const events = await getScheduleEvents(domain, {
                 startTime: createStartOfTime(dateTime),
                 endTime: createEndOfTime(dateTime),
             });
             const schedule = factory.createEvents(events);
-            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTOR.PREVIOUS_BUSINESS_DAY, schedule);
+            templateText = templateText.replaceAll(SPECIAL_TEMPLATE_CHARACTER.PREVIOUS_BUSINESS_DAY_EVENTS, schedule);
         }
 
         return templateText;
