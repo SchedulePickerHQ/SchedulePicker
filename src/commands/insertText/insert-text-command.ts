f0d00c73d89b1beace6d0f7cf4d15675371ee5a1
Atsusuke <atsusuke-murata@cybozu.co.jp>
2021-09-23 15:14:39 +0900

    Rename schedule to insertText.

--- src/commands/insertText/insert-text-command.ts
+++ src/commands/insertText/insert-text-command.ts
@@ -1,27 +1,27 @@
 import { Menus, Tabs } from 'webextension-polyfill';
 import { assertExists } from '../../utils/asserts';
 import { LOADING_STATUS } from '../../utils/loading';
-import { sendLoadingStatus, sendSchedule, sendErrorMessage } from '../sender';
+import { sendLoadingStatus, sendInsertText, sendErrorMessage } from '../sender';
 import { Command } from '../base/command';
 
 export abstract class InsertTextCommand extends Command {
-    async execute(info: Menus.OnClickData, tab: Tabs.Tab) {
+    async execute(_info: Menus.OnClickData, tab: Tabs.Tab) {
         assertExists(tab.id);
         assertExists(tab.url);
         const domain = new URL(tab.url).hostname;
         await sendLoadingStatus(tab.id, LOADING_STATUS.SHOW);
 
         try {
-            const schedule = await this.createInsertText(domain);
+            const text = await this.createInsertText(domain);
 
-            if (schedule === null) {
+            if (text === null) {
                 await sendLoadingStatus(tab.id, LOADING_STATUS.HIDE);
                 return;
             }
 
-            await sendSchedule(tab.id, schedule);
+            await sendInsertText(tab.id, text);
         } catch (error: unknown) {
-            await sendErrorMessage(tab.id, '通信に失敗しました');
+            await sendErrorMessage(tab.id, '予定の取得に失敗しました');
             throw error;
         } finally {
             await sendLoadingStatus(tab.id, LOADING_STATUS.HIDE);
