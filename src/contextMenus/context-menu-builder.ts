b0c039ba022643d8a285f050431f4602d3ce48e8
Atsusuke <atsusuke-murata@cybozu.co.jp>
2021-09-25 13:54:48 +0900

    Set logo icon.

--- src/contextMenus/context-menu-builder.ts
+++ src/contextMenus/context-menu-builder.ts
@@ -34,7 +34,7 @@ export class ContextMenuBuilder {
     constructor() {
         const root: ContextMenuItem = {
             id: CONTEXT_MENU_ID.ROOT,
-            title: 'SchedulePicker beta',
+            title: 'SchedulePicker',
             type: 'normal',
             contexts: ['editable'],
         };
