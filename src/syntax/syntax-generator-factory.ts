bbdc775f33b9301a76732dc9a198e43cab5d1cf7
Atsusuke <atsusuke-murata@cybozu.co.jp>
2021-10-02 23:34:21 +0900

    Fix syntax interface.

---
+++ src/syntax/syntax-generator-factory.ts
@@ -0,0 +1,17 @@
+import { Factory } from '../utils/factory';
+import { SyntaxGenerator } from './base/abstract-syntax';
+import { HtmlSyntaxGenerator } from './html-syntax-generator';
+import { MarkdownSyntaxGenerator } from './markdown-syntax-generator';
+
+export class SyntaxGeneratorFactory implements Factory<'html' | 'markdown', SyntaxGenerator> {
+    create(syntax: 'html' | 'markdown'): SyntaxGenerator {
+        switch (syntax) {
+            case 'html':
+                return new HtmlSyntaxGenerator();
+            case 'markdown':
+                return new MarkdownSyntaxGenerator();
+            default:
+                throw new Error('Syntax is not implemented.');
+        }
+    }
+}
