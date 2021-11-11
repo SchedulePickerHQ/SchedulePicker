c856276e04cfa7b9145c1c86baa48797b3dde993
Atsusuke <atsusuke-murata@cybozu.co.jp>
2021-11-11 14:25:40 +0900

    Toolbar settings.

--- src/ui/editor/editor.ts
+++ src/ui/editor/editor.ts
@@ -10,7 +10,28 @@ const OPTIONS = {
     debug: 'info',
     placeholder: PLACEHOLDER,
     theme: 'snow',
-} as const;
+    modules: {
+        toolbar: [
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            ['bold', 'italic', 'underline', 'strike'],
+            [{ 'color': [] }, { 'background': [] }, 'blockquote', 'code-block'],
+            ['link', { 'align': [] }, 'clean'],
+        ],
+    },
+    formats: [
+        'background',
+        'bold',
+        'color',
+        'italic',
+        'link',
+        'strike',
+        'underline',
+        'blockquote',
+        'header',
+        'align',
+        'code-block',
+    ],
+};
 
 export const createEditor = (el: HTMLElement): Editor => {
     const editor = new Quill(el, OPTIONS);
