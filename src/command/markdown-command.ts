15d5f73ef25aeaf3e08a33cd2f30dd3bc3396033
Atsusuke <atsusuke-murata@cybozu.co.jp>
2022-06-21 18:11:34 +0900

    記法を切り替えたときはメニューを作り直す

--- src/command/markdown-command.ts
+++ src/command/markdown-command.ts
@@ -1,8 +1,10 @@
+import { buildContextMenu } from '../send-message/to-background';
 import { setSyntax } from '../storage';
 import { AbstractCommand } from './abstract-command';
 
 export class MarkdownCommand extends AbstractCommand {
     async execute() {
         await setSyntax('markdown');
+        await buildContextMenu();
     }
 }
--- src/command/plane-text-command.ts
+++ src/command/plane-text-command.ts
@@ -1,8 +1,10 @@
+import { buildContextMenu } from '../send-message/to-background';
 import { setSyntax } from '../storage';
 import { AbstractCommand } from './abstract-command';
 
 export class PlaneTextCommand extends AbstractCommand {
     async execute() {
         await setSyntax('planeText');
+        await buildContextMenu();
     }
 }
