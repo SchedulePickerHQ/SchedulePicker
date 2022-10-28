23eda5d8d4ef409ebeb1d52c445afb15ce700b55
moyashimaru <17245737+kuroppe1819@users.noreply.github.com>
2022-10-29 01:10:42 +0900

    Display the day of week in placeholders.

--- src/syntax/html-syntax-generator.ts
+++ src/syntax/html-syntax-generator.ts
@@ -1,12 +1,15 @@
 import { i18n } from 'webextension-polyfill';
 import { Event, Member, MyGroupEvent } from '../api/schedule';
-import { DateTime } from '../util/date-time';
+import { DateTime, getDayOfWeek } from '../util/date-time';
 import { AbstractSyntaxGenerator } from './abstract-syntax-generator';
 import { getEventMenuColorCode } from './event-menu-color';
 
 export class HtmlSyntaxGenerator extends AbstractSyntaxGenerator {
     createTitle(dateTime: DateTime) {
-        return `<span>[ ${i18n.getMessage('event_title', dateTime.format('YYYY-MM-DD'))} ]</span>`;
+        return `<span>[ ${i18n.getMessage(
+            'event_title',
+            `${dateTime.format('YYYY-MM-DD')}(${getDayOfWeek(dateTime)})`,
+        )} ]</span>`;
     }
 
     createEvent(domain: string, event: Event | MyGroupEvent) {
