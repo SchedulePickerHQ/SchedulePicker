21e46f998aea90553640709c69702dc674419c98
kuroppe <17245737+kuroppe1819@users.noreply.github.com>
2024-01-09 09:58:59 +0900

    過去に対応していたメニューの分岐と個人設定のメニューの配色を設定

--- src/syntax/event-menu-color.ts
+++ src/syntax/event-menu-color.ts
@@ -47,7 +47,46 @@ export const getEventMenuColorCode = (eventMenu: string): string => {
             return '#646600';
         case '終日':
             return '#009688';
+        // 過去に対応していたメニューの分岐 ここから
+        case '打合':
+            return '#3182dc';
+        case '会議':
+            return '#00508a';
+        case '往来訪(Web)':
+            return '#556b81';
+        case '来訪':
+            return '#57b3ed';
+        case '往訪':
+            return '#f194a7';
+        case '出張':
+            return '#c242aa';
+        case '休み':
+        case '複業':
+            return '#f44848';
+        case 'ウルトラワーク':
+        case 'リモートワーク':
+        case '出社':
+            return '#ef9201';
+        case '勉強会':
+            return '#646600';
+        case '説明会':
+        case '面接':
+            return '#725bce';
+        case 'フェア':
+        case 'セミナー':
+        case '取材/講演':
+            return '#dfc506';
+        case '懇親会':
+        case '社内イベント':
+            return '#158084';
+        case 'タスク':
+        case 'その他':
+            return '#999';
+        case '【履歴】来訪':
+        case '【履歴】往訪':
+            return '#873e0e';
+        // 過去に対応していたメニューの分岐 ここまで
         default:
-            return '#212121';
+            return '#00acc1'; // 個人設定で追加したメニューの配色
     }
 };
